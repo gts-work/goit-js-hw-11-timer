3da769316cf420816f0d7157d9aebf97d39beef1
Grygorii Tsaryk <artgrem@gmail.com>
2021-05-12 02:09:29 +0300

    update date in index js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { CountdownTimer } from "./js/plugin-timer";
 
 new CountdownTimer({
     selector: "#timer-1",
-    targetDate: new Date("Jul 17, 2021"),
+    targetDate: new Date("Jun 29, 2021"),
 });
