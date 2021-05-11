a1e9d5be9a0b8bc84e363b9ccd3aad42eedd1b5e
Grygorii Tsaryk <artgrem@gmail.com>
2021-05-12 02:03:52 +0300

    added countdown timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './styles.css';
+import "./styles.css";
+import { CountdownTimer } from "./js/plugin-timer";
+
+new CountdownTimer({
+    selector: "#timer-1",
+    targetDate: new Date("Jul 17, 2021"),
+});
